3cc1f4638c9382f13275ddc5753eb2d1a7d10b02
kapable <skjoldenstore@gmail.com>
2021-11-05 00:28:53 +0900

    add copy link btn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import React  from 'react';
 import long from './images/long.jpg';
 import toMap from './images/to-map.png';
 import './App.css';
 
 function App() {
+  const doCopy = text => {
+    // 흐름 1.
+    if (!document.queryCommandSupported("copy")) {
+      return alert("복사하기가 지원되지 않는 브라우저입니다.");
+    }
+
+    // 흐름 2.
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    textarea.style.top = 0;
+    textarea.style.left = 0;
+    textarea.style.position = "fixed";
+
+    // 흐름 3.
+    document.body.appendChild(textarea);
+    // focus() -> 사파리 브라우저 서포팅
+    textarea.focus();
+    // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+    textarea.select();
+    // 흐름 4.
+    document.execCommand("copy");
+    // 흐름 5.
+    document.body.removeChild(textarea);
+    alert("클립보드에 복사되었습니다.");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={long} className="long" alt="logo" />
         <div className="invite">
-          <h2>모시는 글</h2>
+          <h2 style={{marginTop:"3rem"}}>모시는 글</h2>
           <p>그동안 보내주신 응원과 후의에
           <br/>
           진심으로 감사드립니다.
@@ -49,6 +76,9 @@ function App() {
           </p>
         </div>
         <hr className="line"/>
+        <button
+          className="copyBtn"
+          onClick={() => doCopy("https://kapable.github.io/everything/")}>✉🔗 모바일 초대장 링크 복사하기</button>
         <a
           className="App-link"
           href="https://reactjs.org"
